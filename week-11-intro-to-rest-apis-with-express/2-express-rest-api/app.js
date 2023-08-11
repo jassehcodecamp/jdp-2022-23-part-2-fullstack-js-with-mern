442e0527ca6fa4ebacfee63623297c14dca5e1ea
Omar Jasseh <jassehomar99@gmail.com>
2023-08-11 19:19:22 +0000

    week-11 complete. Basic Node/Express Rest API with validations

--- week-11-intro-to-rest-apis-with-express/2-express-rest-api/app.js
+++ week-11-intro-to-rest-apis-with-express/2-express-rest-api/app.js
@@ -1,12 +1,21 @@
 import express from "express"
+import crypto from "crypto"
 
 const app = express()
 const port = 3000
 
 app.use(express.json()) // for parsing application/json
 
+const myMiddleware = (req, res, next) => {
+  console.log("New request")
+  // req.body.test = "My test"
+  next()
+}
+
+app.use(myMiddleware)
 const contacts = [
   {
+    id: 1,
     name: "Omar Jasseh",
     email: "omar@jassehcodecamp.com",
     phone: 3100948,
@@ -14,6 +23,7 @@ const contacts = [
   },
 
   {
+    id: 2,
     name: "Buba Conteh",
     email: "buba@jassehcodecamp.com",
     phone: 3104015,
@@ -33,13 +43,100 @@ app.get("/contacts", (req, res) => {
 
 // Get a single Contact through a Route Parameter
 app.get("/contacts/:id", (req, res) => {
-  res.json(contacts[req.params.id])
+  const contact = findContact(req.params.id, res)
+
+  if (!contact) {
+    return res.status(404).json({
+      status: "failure",
+      message: `The Contact with the Id ${req.params.id} was not found!`,
+    })
+  }
+  return res.json(contact)
 })
 
 // Store a Contact
 app.post("/contacts", (req, res) => {
-  res.json(req.body)
+  // validate Contact data
+  const validatedData = validateContact(req.body)
+  if (!validatedData) {
+    return res.status(422).json({
+      status: "failure",
+      message: "The Name, Phone, and Email are required",
+    })
+  }
+
+  const contact = { ...validatedData, id: crypto.randomUUID() }
+  contacts.push(contact)
+  return res.status(201).json({
+    status: "success",
+    message: "Contact created successfully",
+    contact: contact,
+  })
 })
+
+// Update a Contact
+app.patch("/contacts/:id", (req, res) => {
+  // Find Contact
+  const contact = findContact(req.params.id, res)
+
+  if (!contact) {
+    return res.status(404).json({
+      status: "failure",
+      message: `The Contact with the Id ${req.params.id} was not found!`,
+    })
+  }
+  // Validate Contact data
+  const validatedData = validateContact(req.body, res)
+
+  if (!validatedData) {
+    return res.status(422).json({
+      status: "failure",
+      message: "The Name, Phone, and Email are required",
+    })
+  }
+
+  const { name, phone, email, address } = validatedData
+  // Update Contact
+  contact.name = name
+  contact.phone = phone
+  contact.email = email
+  contact.address = address
+
+  return res.status(200).json({
+    status: "success",
+    message: "Contact updated successfully",
+    contact: contact,
+  })
+})
+
+// Delete Contact
+app.delete("/contacts/:id", (req, res) => {
+  const contact = findContact(req.params.id)
+  if (!contact) {
+    return res.status(404).json({
+      status: "failure",
+      message: `The Contact with the Id ${req.params.id} was not found!`,
+    })
+  }
+
+  const contactIndex = contacts.indexOf(contact)
+  contacts.splice(contactIndex, 1)
+
+  res.status(204).end()
+})
+
+function findContact(contactId, res) {
+  return contacts.find((c) => c.id == contactId)
+}
+
+function validateContact(contact) {
+  const { name, phone, email } = contact
+  if (!name || !phone || !email) {
+    return false
+  }
+
+  return contact
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
